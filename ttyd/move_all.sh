66c34872d2a798abc37e0e0206ad14f11bdd38f2
peytonzhu <48082092+peytonzhu@users.noreply.github.com>
2021-12-10 22:31:49 +0800

    Update move_all.sh

--- ttyd/move_all.sh
+++ ttyd/move_all.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 #=================================================
-if ls /home/downloads/*.torrent >/dev/null 2>&1;then
-
+if ls /home/downloads/*.aria2 >/dev/null 2>&1;then
 str1="/home/"
 str2=`date +%H%M%S`
 str3=$str1$str2
 mkdir $str3
 mv /home/downloads/* $str3
 rclone copy $str3 ensp1:/rclone --exclude "*.torrent"  --min-size 10
-sleep 2
-rclone copy $str3 ensp1:/rclone --exclude "*.torrent"  --min-size 10
-
-#
-#fi
+fi
