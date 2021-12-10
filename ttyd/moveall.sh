eabf28a72d9fe56352cca9f983d7e79a59a03966
peytonzhu <48082092+peytonzhu@users.noreply.github.com>
2021-12-10 22:50:22 +0800

    Create moveall.sh

---
+++ ttyd/moveall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#=================================================
+if ls /home/downloads/*.aria2 >/dev/null 2>&1
+then
+touch have.temp
+else
+str1="/home/"
+str2=`date +%H%M%S`
+str3=$str1$str2
+mkdir $str3
+mv /home/downloads/* $str3
+rclone copy $str3 ensp1:/rclone --exclude "*.torrent"  --min-size 10
+fi
