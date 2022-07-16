0061a20127ad4ace150e6223a20c996f20b3338b
peytonzhu <48082092+peytonzhu@users.noreply.github.com>
2022-07-16 16:37:26 +0800

    Update moveall.sh

--- ttyd/moveall.sh
+++ ttyd/moveall.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 #=================================================
-sleep 5
-if ls /home/downloads/*.aria2 >/dev/null 2>&1
-then
-touch have.temp
-else
-str1="/home/"
-str2=`date +%H%M%S`
-str3=$str1$str2
-mkdir $str3
-mv /home/downloads/* $str3
-rclone copy $str3 ensp1:/rclone --exclude "*.torrent"  --min-size 10
-fi
+#sleep 5
+#if ls /home/downloads/*.aria2 >/dev/null 2>&1
+#then
+##touch have.temp
+#else
+#str1="/home/"
+#str2=`date +%H%M%S`
+#str3=$str1$str2
+#mkdir $str3
+#mv /home/downloads/* $str3
+#rclone copy $str3 ensp1:/rclone --exclude "*.torrent"  --min-size 10
+#fi
+rm /bin/rclone.conf
+mv rclone.conf /bin/
+rclone copy downloads ensp1:/rclone/220716 -P
